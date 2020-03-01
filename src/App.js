70b99ff8a0dd1f874364aebabdd3595dc97db415
Phil at Xonox <36802525+CodeTrafficker@users.noreply.github.com>
2020-02-29 23:37:31 -0800

    Update App.js
    Many working console logs, but sadly, broken. Confusion where this.state.quotes is set, getting setState() to update, etc

--- src/App.js
+++ src/App.js
@@ -19,8 +19,10 @@ class App extends React.Component {
   constructor(props)  {
     super(props);
     this.state = {
-      value: "",
-      quotes: [],
+      quotes: {
+        quote: '',
+        author: '',
+      },
     };
     // Handles here.
   }
@@ -39,40 +41,86 @@ class App extends React.Component {
         console.log('In FIRST THEN: ', typeof this.state.quotes);
 
         let quotesObj = response.data;
-        // // console.log('no4 quote: ', Object.entries(quotesObj)[0][1][4].quote )
+        console.log('no4 quote: ', Object.entries(quotesObj)[0][1][4].quote )
         // // console.log('no4 author: ', Object.entries(quotesObj)[0][1][4].author )
         // t.s.quotes is not an iterable object, so 
-        // have to do this - push the bits to an array:
+        // have to do this - push the bits to an iterable array:
         const quoteEntries = Object.entries(quotesObj)[0][1];
         for(quoAuth of quoteEntries ){ 
           console.log('Quote: ', quoAuth.quote); 
           console.log('Author: ', quoAuth.author); 
           quoteArr.push(quoAuth);
         }
+
         this.setState({quotes: quoteArr});
-        console.log('What is this.state.quotes now: ', Array.isArray(this.state.quotes)
-         +', [3]: ', this.state.quotes[3].quote );
+
+        console.log('Is this.state.quotes an array: ', Array.isArray(this.state.quotes) );
       })
       .catch(err => console.log(err))
   }
 
 
-  render() {
-    let quotesRen = this.state.quotes;
-    console.log(' quotes is an array? ', Array.isArray(this.state.quotes) +
-      ', No 17 in render: ', quotesRen[17] )
 
-    return (
-      <div className="App">
-        <h1>Here: </h1>
+  render() {
+    let allQuotes = this.state.quotes;
+
+    const randomizer = (max) => {
+      return Math.floor(Math.random() * (max) )
+    }
+/*
+    function AllQuotesMod(props) {
+      return(
         <div className="container">
-          {this.state.quotes.map((quote, index) => (
-            <div className="quoteCont" key={'q'+index}>
+          {allQuotes.map((quote, index) => (
+            <div className="quoteCont" key={index}>
               <p>{index+1}] Quote: {quote.quote}<br />
               Author: {quote.author}</p>
             </div>
           ))}
         </div>
+      )
+    }
+*/
+
+    console.log('this.state.quotes at 85: ', typeof this.state.quotes[0] )
+
+    function RandomQuote(props) {
+      let randoNum = randomizer( allQuotes.length );
+      // const randQuote = allQuotes[randoNum];
+      const randQuote = allQuotes[randoNum];
+
+      console.log('allQuotes.length: '+ allQuotes.length )
+      // 102
+
+      return(
+        <div className="container"> 
+          randoNum: {randoNum} 
+           
+          <div className="quoteCont">
+            <p> 
+              Quote: {randQuote}
+              <br />
+              Author: xxxxxx 
+            </p>
+          </div> 
+        </div>
+      )
+    }
+
+
+    console.log(' quotes is an array? ', Array.isArray(this.state.quotes) +
+      ', No 17 in render: ', allQuotes[17] )
+
+    return (
+      <div className="App">
+        <h1>Here: randoNum is {RandomQuote.randoNum} </h1>
+        
+        RandomQuote------------ 
+        <RandomQuote /> 
+        END RandomQuote-----------
+
+         ||  AllQuotesMod /|| 
+
       </div>
     );
   }
